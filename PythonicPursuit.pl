c50fc2426b0c9996f31333599ef094e297d8ef54
SoLowMow <110025570+solowmow@users.noreply.github.com>
2023-11-17 17:27:55 +0100

    Create PythonicPursuit.pl

---
+++ PythonicPursuit.pl
@@ -0,0 +1,16 @@
+# Perl isn't best suited for game development due to its lack of native graphical support.
+
+# Example pseudo-code representation
+sub initializeGame {
+    # Initialize variables
+}
+
+sub gameLoop {
+    while ($gameIsRunning) {
+        # Get user input
+        # Update snake position
+        # Check collision
+        # Generate fruit if eaten
+        # Render game (text-based representation)
+    }
+}
